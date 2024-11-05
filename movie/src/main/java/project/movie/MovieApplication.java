08c53f182371a288737ef23a547e60e477242539
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2024-11-05 17:17:34 +0900

    CHORE : 스프링부트 패키지 업로드

---
+++ movie/src/main/java/project/movie/MovieApplication.java
@@ -0,0 +1,14 @@
+package project.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(MovieApplication.class, args);
+	}
+
+}
