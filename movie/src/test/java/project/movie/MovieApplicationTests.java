fe2f63a387b2204736a5e55830d6c1672a6b7ef7
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2024-11-06 15:05:24 +0900

    [FEAT] : Member API Junit 파일 추가

--- movie/src/test/java/project/movie/MovieApplicationTests.java
+++ movie/src/test/java/project/movie/MovieApplicationTests.java
@@ -1,9 +1,16 @@
 package project.movie;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "http://localhost:8080", description = "Default Server URL")
+		}
+)
 class MovieApplicationTests {
 
 	@Test
