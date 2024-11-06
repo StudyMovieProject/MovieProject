74cd7b83e2ecbfda7b2527f23672b3744e8cb2dd
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2024-11-06 15:04:38 +0900

    [FEAT] : Member API 추가 1. 회원 가입 2. 회원 수정 3. 회원 조회 4. 회원 삭제 5. 로그인 유저 정보 조회 6. 회원 목록 조회

---
+++ movie/src/main/java/project/movie/member/dto/MemberUpdateReqDto.java
@@ -0,0 +1,72 @@
+package project.movie.member.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import project.movie.member.domain.Member;
+import project.movie.member.domain.MemberRole;
+import project.movie.member.domain.MemberStatus;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class MemberUpdateReqDto {
+    @NotNull
+    private String memberId;
+    @NotNull
+    @Size(min = 4, max = 20)
+    private String password;
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요")
+    private String username;
+    @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z0-9]{2,20}@[a-zA-Z0-9]{2,10}\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해주세요")
+    private String email;
+    @NotEmpty
+    @Pattern(regexp = "^[0-9]{11}", message = "연락처 형식으로 작성해주세요")
+    private String tel;
+    private String zipcode;
+    private String address;
+    private String detailAddress;
+    private String fullname;
+    @NotNull
+    // @Pattern(regexp = "^(ADMIN|CUSTOMER)$")
+    private MemberRole role; // ADMIN, CUSTOMER
+    private MemberStatus status; // 활동 여부
+    private String certificationCode; // 인증 코드
+
+    @Builder
+    public MemberUpdateReqDto(Member member) {
+        this.memberId = member.getMemberId();
+        this.password = member.getPassword();
+        this.username = member.getUsername();
+        this.email = member.getEmail();
+        this.tel = member.getTel();
+        this.zipcode = member.getZipcode();
+        this.address = member.getAddress();
+        this.detailAddress = member.getDetailAddress();
+        this.fullname = member.getFullname();
+        this.role = member.getRole();
+        this.status = member.getStatus();
+    }
+
+    public Member to(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        return Member.builder()
+                .memberId(this.memberId)
+                .password(bCryptPasswordEncoder.encode(password))
+                .username(username)
+                .email(email)
+                .tel(tel)
+                .zipcode(zipcode)
+                .address(address)
+                .detailAddress(detailAddress)
+                .fullname(fullname)
+                .role(role)
+                .status(status)
+                .build();
+    }
+}
