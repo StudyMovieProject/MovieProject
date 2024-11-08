d3514aba057868a7db1bd4042698da06934aa78f
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2024-11-08 23:26:46 +0900

    movie 파일 이동

--- movie/src/main/java/project/movie/common/handler/exception/CustomForbiddenException.java
+++
@@ -1,9 +0,0 @@
-package project.movie.common.handler.exception;
-
-
-// 추후에 사용할 예정
-public class CustomForbiddenException extends RuntimeException {
-    public CustomForbiddenException(String message) {
-        super(message);
-    }
-}
